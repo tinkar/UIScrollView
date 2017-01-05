4277999ca223731d302a3eb0f8c20f57c136cdb6
tinkar <kr.kopec@icloud.com>
2017-01-05 12:11:07 +0100

    Initial commit

--- UIScrollView/ViewController.swift
+++ UIScrollView/ViewController.swift
@@ -10,15 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var images = [UIImageView]()
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        for x in 0...2 {
+            
+            let image = UIImage(named: "icon\(x).png")
+            images.append(UIImageView(image: image))
+            
+            
+        }
+        
+        print("count: \(images.count)")
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+ 
 
 
 }
