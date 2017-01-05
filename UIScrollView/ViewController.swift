23e5c28457524c18024f508143534b1e256edfc0
tinkar <kr.kopec@icloud.com>
2017-01-05 13:17:17 +0100

    next item in scrollview visible
    modified the app to show a part of the next item in the scrollview so that the user knows they need to scroll right

--- UIScrollView/ViewController.swift
+++ UIScrollView/ViewController.swift
@@ -21,12 +21,24 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+    }
+
+    
+    override func viewDidAppear(_ animated: Bool) {
+    // the code was moved from viewDidLoad to viewDidAppear becuase viewDidLoad function does not provide the value of the width of the scrollview and viewDidAppear does
+        
+        
         var contentWidth: CGFloat = 0.0
+        
+        
         
         for x in 0...2 {
             
             let image = UIImage(named: "icon\(x).png")
             // connects the file to the current image variable (to be changed with the next iteration of the loop
+            
+            let scrollViewWidth = scrollView.frame.size.width
             
             let imageView = UIImageView(image: image)
             images.append(imageView)
@@ -35,7 +47,7 @@ class ViewController: UIViewController {
             var newX: CGFloat = 0.0
             // sets the initial value for the center of the image in X axis
             
-            newX = view.frame.midX + view.frame.size.width * CGFloat(x)
+            newX = scrollViewWidth / 2 + scrollViewWidth * CGFloat(x)
             // sets the center of the image in X axis to be in the center of the screen plus the width of the screen multiplied by the number of images(each image corresponds to screen 0, 1 and 2)
             
             contentWidth += newX
@@ -44,16 +56,18 @@ class ViewController: UIViewController {
             scrollView.addSubview(imageView)
             // adds the image view to the main view
             
-            imageView.frame = CGRect(x: newX - 75, y: (view.frame.size.height / 2) - 75, width: 150, height: 150)
+            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height / 2) - 75, width: 150, height: 150)
             // sets the position of the image view and its dimensions
             
             
         }
         
+        scrollView.clipsToBounds = false
+        // prevents clipping the images when scrolling
+        
         scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
         // sets the final value of the size of the scroll view
     }
-
  
 
 
