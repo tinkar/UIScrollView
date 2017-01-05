f35a23c83d5d3133aa5b3f38e099978a44771ab9
tinkar <kr.kopec@icloud.com>
2017-01-05 12:55:00 +0100

    Finalized the app

--- UIScrollView/ViewController.swift
+++ UIScrollView/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var scrollView: UIScrollView!
+    
+    
     var images = [UIImageView]()
     
     
@@ -18,16 +21,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        var contentWidth: CGFloat = 0.0
+        
         for x in 0...2 {
             
             let image = UIImage(named: "icon\(x).png")
-            images.append(UIImageView(image: image))
+            // connects the file to the current image variable (to be changed with the next iteration of the loop
+            
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            // creates an image view with the current image in it
+            
+            var newX: CGFloat = 0.0
+            // sets the initial value for the center of the image in X axis
+            
+            newX = view.frame.midX + view.frame.size.width * CGFloat(x)
+            // sets the center of the image in X axis to be in the center of the screen plus the width of the screen multiplied by the number of images(each image corresponds to screen 0, 1 and 2)
+            
+            contentWidth += newX
+            // sets the width of the content inside the scrollview to be compliant with the current value of newX
+            
+            scrollView.addSubview(imageView)
+            // adds the image view to the main view
+            
+            imageView.frame = CGRect(x: newX - 75, y: (view.frame.size.height / 2) - 75, width: 150, height: 150)
+            // sets the position of the image view and its dimensions
             
             
         }
         
-        print("count: \(images.count)")
-        
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+        // sets the final value of the size of the scroll view
     }
 
  
